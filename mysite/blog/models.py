7ed1eb8bf309a11e08af5a1543043b02b870dc68
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-03-16 23:14:09 +0530

    modify Comment class model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
     self.save()
   
   def approve_comments(self):
-    return self.comments.filter(approved_comments=True)
+    return self.comments.filter(approved_comment=True)
 
   def __str__(self):
     return self.title
@@ -28,3 +28,10 @@ class Comment(model.Model):
   text = models.TextField()
   create_date = models.DateTimeField(dafault=timezone.now())
   approved_comment = models.BooleanField(default=False)
+
+  def approve(self):
+    self.approved_comment = True
+    self.save()
+
+  def __str__(self):
+    return self.text
