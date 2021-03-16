d4af403b82744702a28f57c42164305c63080a36
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-03-16 22:09:53 +0530

    create Comment class model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -20,3 +20,11 @@ class Post(models.Model):
 
   def __str__(self):
     return self.title
+
+
+class Comment(model.Model):
+  post = models.ForeignKey('blog.Post',related_name='comments')
+  author = models.CharField(max_length=200)
+  text = models.TextField()
+  create_date = models.DateTimeField(dafault=timezone.now())
+  approved_comment = models.BooleanField(default=False)
