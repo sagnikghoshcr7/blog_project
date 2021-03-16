7483cc487049ebd97902c816f774ee0aece4de73
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-03-16 22:01:12 +0530

    create Post class model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.core.urlresolvers import reverse
 
 # Create your models here.
+
+class Post(models.Model):
+  author = models.ForeignKey('auth.User')
+  title = models.CharField(max_length=200)
+  text = models.TextField()
+  create_date = models.DateTimeField(default=timezone.now())
+  published_date = models.DateTimeField(blank=True,null=True)
+
+  def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+  
+  def approve_comments(self):
+    return self.comments.filter(approved_comments=True)
+
+  def __str__(self):
+    return self.title
