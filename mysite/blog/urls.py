7fe3fcbd51b3275b071183853d2fcd1eaff08377
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-03-17 02:47:43 +0530

    update views and urls

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
   url(r'^post/(?P<pk>\d+)/edit/$',views.PostUpdateView.as_view(),name='post_edit'),
   url(r'^post/(?P<pk>\d+)/remove/$',views.PostDeleteView.as_view(),name='post_remove'),
   url(r'^drafts/$',views.DraftListView.as_view(),name='post_draft_list'),
+  url(r'^post/(?P<pk>\d+)/comment/$',views.add_comment_to_post,name='add_comment_to_post'),
+  url(r'^comment/(?P<pk>\d+)/approve/$',views.comment_approve,name='comment_approve'),
+  url(r'^comment/(?P<pk>\d+)/remove/$',views.comment_remove,name='comment_remove'),
+  url(r'^post/(?P<pk>\d+)/publish/$',views.post_publish,name='post_publish'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -50,6 +50,12 @@ class DraftListView(LoginRequiredMixin,ListView):
 ####################################################
 
 @login_required
+def post_publish(request,pk):
+  post = get_object_or_404(Post,pk=pk)
+  post.publish
+  return redirect('post_detail',pk=pk)
+
+@login_required
 def add_comment_to_post(request,pk):
   post = get_object_or_404(Post,pk=pk)
   if request.method == 'POST':
@@ -62,3 +68,16 @@ def add_comment_to_post(request,pk):
   else:
     form = CommentForm()
   return render(request,'blog/comment_form.html',{'form':form})
+
+@login_required
+def comment_approve(request,pk):
+  comment = get_object_or_404(Comment,pk=pk)
+  comment.approve()
+  return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+  comment = get_object_or_404(Comment,pk=pk)
+  post_pk = comment.post.pk
+  comment.delete()
+  return redirect('post_detail',pk=post_pk)
