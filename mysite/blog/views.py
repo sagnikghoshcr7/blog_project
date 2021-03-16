90dfe1ae3c1be335ffd2d8d9a088376a7efc7a52
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-03-17 02:20:41 +0530

    add login view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.utils import timezone
 from blog.models import Post,Comment
 from blog.forms import PostForm,CommentForm
 from django.urls import reverse_lazy
@@ -43,3 +44,21 @@ class DraftListView(LoginRequiredMixin,ListView):
 
   def get_queryset(self):
     return Post.objects.filter(published_date__isnull=True).order_by('create_date')
+
+
+####################################################
+####################################################
+
+@login_required
+def add_comment_to_post(request,pk):
+  post = get_object_or_404(Post,pk=pk)
+  if request.method == 'POST':
+    form = CommentForm(request.POST)
+    if form.is_valid():
+      comment = form.save(commit=False)
+      comment.post = post
+      comment.save()
+      return redirect('post_detail', pk=post.pk)
+  else:
+    form = CommentForm()
+  return render(request,'blog/comment_form.html',{'form':form})
