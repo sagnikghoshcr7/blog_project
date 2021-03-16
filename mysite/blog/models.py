ea5adcca3373b4976e3106d4b50971784ad26414
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-03-16 23:27:51 +0530

    modify model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -18,6 +18,9 @@ class Post(models.Model):
   def approve_comments(self):
     return self.comments.filter(approved_comment=True)
 
+  def get_absolute_url(self):
+    return reverse("post_detail",kwargs={'pk':self.pk})
+
   def __str__(self):
     return self.title
 
@@ -32,6 +35,9 @@ class Comment(model.Model):
   def approve(self):
     self.approved_comment = True
     self.save()
+
+  def get_absolute_url(self):
+    return reverse('post_list')
 
   def __str__(self):
     return self.text
