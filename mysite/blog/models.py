328a6e427c9da22cbb907ecace775c46be4408b2
Srijita Mallick <msrijita189@gmail.com>
2021-04-12 13:28:40 +0530

    change in login url + model time

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from django.utils.timezone import now
 
 # Create your models here.
 
@@ -8,7 +9,7 @@ class Post(models.Model):
   author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
   title = models.CharField(max_length=200)
   text = models.TextField()
-  create_date = models.DateTimeField(default=timezone.now())
+  create_date = models.DateTimeField(default=now)
   published_date = models.DateTimeField(blank=True,null=True)
 
   def publish(self):
@@ -29,7 +30,7 @@ class Comment(models.Model):
   post = models.ForeignKey('blog.Post',related_name='comments',on_delete=models.CASCADE)
   author = models.CharField(max_length=200)
   text = models.TextField()
-  create_date = models.DateTimeField(default=timezone.now())
+  create_date = models.DateTimeField(default=now)
   approved_comment = models.BooleanField(default=False)
 
   def approve(self):
